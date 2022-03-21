0a8cc69783b3201eb931c6da2dccb35f9f4e1f08
md-Imteyaz <98022042+md-Imteyaz@users.noreply.github.com>
2022-03-21 18:39:55 +0530

    Add files via upload

---
+++ FlywayApplication1Application.java
@@ -0,0 +1,14 @@
+package com.Employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+@SpringBootApplication
+@ComponentScan
+public class FlywayApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayApplication1Application.class, args);
+	}
+
+}
